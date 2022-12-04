c8bfb9ff098e3690627270f7c29a7ef7e44b12aa
estudantedehtml <mauriciopedeo007@gmail.com>
2022-12-04 02:14:44 -0300

    ajustes

--- src/compontents/layout/NavBar.tsx
+++ src/compontents/layout/NavBar.tsx
@@ -3,12 +3,14 @@ import Logo from '../../assets/logo.png'
 
 export default function Navbar() {
     return (
-        <nav className="px-2 py-4 bg-yellow">
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-                <Link to="/" className="flex items-center">
-                    <img src={Logo} className="h-6 mr-3 sm:h-10" alt="Pokemon Logo" />
-                </Link>
-            </div>
-        </nav>
+        <header>
+            <nav className="px-2 py-4 bg-yellow">
+                <div className="container flex flex-wrap items-center justify-between mx-auto">
+                    <Link to="/" className="flex items-center">
+                        <img src={Logo} className="w-auto h-6 mr-3 sm:h-10" alt="Pokemon Logo" />
+                    </Link>
+                </div>
+            </nav>
+        </header>
     )
 }
